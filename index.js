c7162ed3fee34052f4b44f6abcc6879d8a20e734
Amer Ajjawi <ajjawi_amer@hotmail.com>
2024-08-07 19:30:22 -0400

    set up function for human choice and check if it matches the values in the choice array or not

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let userChoice;
 //set up a function to randomly get the values of choice for the computer
 function getComputerChoice() {
     //needs to return the random choice from array
-    
+
     //assign the choice to computerChoice with math random
     computerChoice = choice[Math.floor(Math.random() * choice.length)];
 
@@ -22,5 +22,29 @@ function getComputerChoice() {
 //call the function 
 getComputerChoice();
 
-//test the output
-console.log(computerChoice);
+//set up a function to validate the human choice through a prompt
+function getHumanChoice() {
+    //add a while loop & validiate user choice's that are in the array 
+    while (choice.includes(userChoice)) {
+        //prompt user choice 
+        userChoice = prompt('please enter rock, paper, or scissor');
+
+        //conditionals to see if the 
+        if (choice.includes(userChoice)){
+
+            //return enter either rock, paper, or scissors
+            return `Your choice was ${userChoice}`;
+        } else {
+            
+            //return the user's choice
+            return `please enter the correct values`;
+        }
+
+    }
+}
+
+//call function
+getHumanChoice();
+
+//test the variable
+console.log(userChoice);
