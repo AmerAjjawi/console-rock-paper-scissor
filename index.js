a14d3697a4b4bdc82bb11c117649df0fc4b61796
Amer Ajjawi <ajjawi_amer@hotmail.com>
2024-08-07 18:16:20 -0400

    set up function for computer choice to return the values in the array randomly and used console.log to test it

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ let computerChoice;
 //initalize human choice variable
 let userChoice;
 
+//set up a function to randomly get the values of choice for the computer
+function getComputerChoice() {
+    //needs to return the random choice from array
+    
+    //assign the choice to computerChoice with math random
+    computerChoice = choice[Math.floor(Math.random() * choice.length)];
+
+    //return the computer Choice
+
+    return computerChoice;
+}
+
+//call the function 
+getComputerChoice();
+
+//test the output
+console.log(computerChoice);
