202e9e04b9fb50a4c72da7520ad897b0059bd9b0
Amer Ajjawi <ajjawi_amer@hotmail.com>
2024-08-07 18:07:29 -0400

    set up variable for choices. Initliazed computer choice variable and human choice variable.

---
+++ index.js
@@ -0,0 +1,9 @@
+//set up a choice for rock, paper, scissor
+let choice = ['rock', 'paper', 'scissor'];
+
+//initalize computer choice variable
+let computerChoice;
+
+//initalize human choice variable
+let userChoice;
+
