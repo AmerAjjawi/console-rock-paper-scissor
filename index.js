f0ef326d5bb3d16c32041cbba34915410d7571e0
ajjawi_amer@hotmail.com <ajjawi_amer@hotmail.com>
2024-10-14 21:29:43 -0400

    Edited the variables for userChoice and ComputerChoice. Fixed loop function logic

--- index.js
+++ index.js
@@ -16,35 +16,28 @@ let humanScore = 0;
 //initalize result variable
 let result;
 
-//initalize winner
-let winner;
-
 //set up a function to randomly get the values of choice for the computer
 function getComputerChoice() {
   //needs to return the random choice from array
 
   //assign the choice to computerChoice with math random
-  computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  generatedChoice = choices[Math.floor(Math.random() * choices.length)];
 
   //return the computer Choice
 
-  return computerChoice;
+  return generatedChoice;
 }
 
 //set up a function to validate the human choice through a prompt
 function getHumanChoice() {
   //add a while loop & validiate user choice's that are in the array
-  while (!choice.includes(userChoice)) {
+  while (!choices.includes(userChoice)) {
     //prompt user choice
-    userChoice = prompt("please enter rock, paper, or scissors");
+    let userPrompt = prompt("please enter rock, paper, or scissors");
 
-    userChoice = userChoice.toLowerCase();
-    if (choices.includes(userChoice)) {
-      return userChoice;
-    } else {
-      return "Please enter a value to play ";
-    }
+    //userChoice = userChoice.toLowerCase();
   }
+  return userPrompt;
 }
 
 //set up a function that gets the computer and human choice as parameters and play one round
@@ -58,11 +51,11 @@ function playRound(human, computer) {
   //check for rock
   if (human === "rock") {
     if (computer === "paper") {
-      //computerScore++;
+      computerScore++;
       result = `You lose! Paper beats Rocks`;
       return result;
     } else if (computer === "scissors") {
-      //humanScore++;
+      humanScore++;
       result = `You win! Rock beats Scissors`;
       return result;
     }
@@ -70,58 +63,51 @@ function playRound(human, computer) {
   //check for paper
   if (human === "paper") {
     if (computer === "rock") {
-      //humanScore++;
+      humanScore++;
       result = `You win! Paper beats Rock`;
       return result;
-    } else if (computer === "scissor") {
-      // computerScore++;
-      result = `You lose! Scissors beats Rock `;
+    } else if (computer === "scissors") {
+      computerScore++;
+      result = `You lose! Scissors beats Paper `;
       return result;
     }
   }
   // check for scissor
-  if (human === "scissor") {
+  if (human === "scissors") {
     if (computer === "paper") {
-      //humanScore++;
+      humanScore++;
       result = `You win! Scissors beats Paper `;
       return result;
     } else if (computer === "rock") {
-      //computer++;
+      computer++;
       result = `You lose! Rock beats Scissors`;
       return result;
     }
   }
 }
 
-//placing human function in new variable
-const humanSelection = getHumanChoice();
-
-//placing computer function in new variable
-const computerSelection = getComputerChoice();
-
 //function to play five rounds
 
-function fiveRoundsGame() {
-  //use a for loop
-  for (let i = 0; i < 5; i++) {
-    //call function inside
-    winner = playRound(humanSelection, computerSelection) * 3;
-    if (winner.includes("win")) {
-      player++;
-      console.log(
-        `Human Score: ${humanScore} | Computer Score: ${computerScore}  `
-      );
-    } else if (winner.includes("lose")) {
-      computerScore++;
-      console.log(
-        `Human Score ${humanScore} | Computer Score ${computerScore} `
-      );
-    }
+function threeRounds() {
+  //Display score of who ever reaches 3 rounds
+  while (humanScore < 3 && computerScore < 3) {
+    //placing human function in new variable
+    userChoice = getHumanChoice();
+
+    //placing computer function in new variable
+    computerChoice = getComputerChoice();
+    playRound(userChoice, computerChoice);
+    userChoice = "";
+    computerChoice = "";
   }
   if (humanScore > computerScore) {
-    return "Human won this round";
+    console.log(
+      `Human won! Human Score: ${humanScore} | Computer Score: ${computerScore}`
+    );
   } else {
-    return "Better luck next time!";
+    console.log(
+      `Human lost! Human Score: ${humanScore} | Computer Score: ${computerScore}`
+    );
   }
 }
-fiveRoundsGame();
+threeRounds();
